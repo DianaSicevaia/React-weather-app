760ce07dd44d2945909cf5927ef01d9679b52e1e
Diana Sicevaia <sicevaiadiana@gmail.com>
2021-01-24 14:42:25 +0200

    added axios and useEffect hook

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,23 @@
-import React from "react"
+import React, {useEffect} from "react"
+import axios from "axios"
+
 import Header from "./Header"
 
 const API_KEY = "855195a766925822099e1656ec8594e0"
-const url = "api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}"
+
 
 const Main = () => {
+    const api_call = async () => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=Chișinău&appid=${API_KEY}`
+        const request = axios.get(url)
+        const response = await request
+        console.log(response)
+    }
+
+    useEffect(() => {
+        api_call()
+    }, [])
+
     return (
         <div className="main">
             <Header/>
