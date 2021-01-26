be1471e8dcd1d582cd3344ae5e2d614b307c6cae
Diana Sicevaia <sicevaiadiana@gmail.com>
2021-01-26 19:11:16 +0200

    added two new components: Content and WeatherSearch

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,25 +2,30 @@ import React, {useEffect} from "react"
 import axios from "axios"
 
 import Header from "./Header"
-
-const API_KEY = "855195a766925822099e1656ec8594e0"
+import Content from "./Content"
+import WeatherSearch from "./WeatherSearch"
 
 
 const Main = () => {
-    const api_call = async () => {
+    const api_call = async e => {
+        // e is the event in JS. This e.preventDefault()
+        // will prevent the refresh of the page in the browser
+        e.preventDefault()
+        
+        const API_KEY = "855195a766925822099e1656ec8594e0"
         const url = `https://api.openweathermap.org/data/2.5/weather?q=Chișinău&appid=${API_KEY}`
         const request = axios.get(url)
         const response = await request
         console.log(response)
     }
 
-    useEffect(() => {
-        api_call()
-    }, [])
 
     return (
         <div className="main">
             <Header/>
+            <Content>
+                <WeatherSearch api_call={api_call}/>
+            </Content>
         </div>
     )
 }
