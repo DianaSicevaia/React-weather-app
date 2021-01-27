3ba964b94064cbb2fe49134c184bb7ad33601f37
Diana Sicevaia <sicevaiadiana@gmail.com>
2021-01-27 16:37:03 +0200

    made the url more dynamic

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,18 +11,21 @@ import WeatherData from "./WeatherData"
 
 const Main = () => {
     const[weather, setWeather] = useState()
+    const [city, setCity] = useState()
 
     const api_call = async e => {
         // e is the event in JS. This e.preventDefault()
         // will prevent the refresh of the page in the browser
         e.preventDefault()
         
+        const location = e.target.elements.location.value
         const API_KEY = "855195a766925822099e1656ec8594e0"
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=Chișinău&appid=${API_KEY}&units=metric`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
         const request = axios.get(url)
         const response = await request
 
         setWeather(response.data.main)
+        setCity(response.data.name)
     }
 
   weather && console.log(weather)
@@ -32,7 +35,7 @@ const Main = () => {
         <div className="main">
             <Header/>
             <Content>
-              <Context.Provider value={{ api_call, weather }}>
+              <Context.Provider value={{ api_call, weather, city }}>
               <WeatherSearch/>
                 { weather && <WeatherData/>}
               </Context.Provider>
