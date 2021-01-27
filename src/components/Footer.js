f19ee7074dad63b3561a1161da5307b70bd6e219
Diana Sicevaia <sicevaiadiana@gmail.com>
2021-01-27 17:34:19 +0200

    added a few components

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Footer =() => (
+    <div className="footer">
+        <p>Thank you for using this app!</p>
+    </div>
+
+)
+
+export default Footer
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,18 @@ import axios from "axios"
 import Context from "../Context"
 
 import Header from "./Header"
+import Tagline from "./Tagline"
 import Content from "./Content"
 import WeatherSearch from "./WeatherSearch"
 import WeatherData from "./WeatherData"
-
+import DateTime from "./DateTime"
+import Error from "./Error"
+import Footer from "./Footer"
 
 const Main = () => {
     const[weather, setWeather] = useState()
     const [city, setCity] = useState()
+    const[error, setError] = useState()
 
     const api_call = async e => {
         // e is the event in JS. This e.preventDefault()
@@ -19,6 +23,7 @@ const Main = () => {
         e.preventDefault()
         
         const location = e.target.elements.location.value
+        if (!location) return setError("Please enter the name of the city."), setWeather(null)
         const API_KEY = "855195a766925822099e1656ec8594e0"
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
         const request = axios.get(url)
@@ -26,6 +31,7 @@ const Main = () => {
 
         setWeather(response.data.main)
         setCity(response.data.name)
+        setError(null)
     }
 
   weather && console.log(weather)
@@ -35,10 +41,14 @@ const Main = () => {
         <div className="main">
             <Header/>
             <Content>
-              <Context.Provider value={{ api_call, weather, city }}>
+                <Tagline/>
+                <DateTime/>
+              <Context.Provider value={{ api_call, weather, city, error }}>
               <WeatherSearch/>
                 { weather && <WeatherData/>}
+                { error && <Error error={error}/>}
               </Context.Provider>
+              <Footer/>
             </Content>
         </div>
     )
